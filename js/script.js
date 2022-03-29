d2971239b521f684620e027d01c0046b16aa1cb7
Kym Reilly <kymreilly@hotmail.com>
2022-03-29 17:46:33 +1030

    base files and Start and Reset button created

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var startCount = 16;
+// //TIMER
+
+// var testTimer =$("#startButton").click(function (e) {
+//     var count = startCount;
+//     setInterval(function () {
+//         $("#stopWatch").html(count);
+//         count--;
+//          if (count<= 0){
+//             $("#stopWatch").html("TIME'S UP!")}
+//     }, 1000);
+// });
+ 
+
+
+
+
+// $("#resetButton").click(function (e) {
+//     clearInterval(testTimer()
+// //     {
+// //      $("#stopWatch").html("Click 'Start' to begin") 
+// // }
+// )
+// });
