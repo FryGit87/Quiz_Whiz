e5464289e21393361481a24f336138b7990fa88c
Kym Reilly <kymreilly@hotmail.com>
2022-03-31 21:04:00 +1030

    slight js changes, variables added for later functions, faviocon added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var btnReset = $("#btn-reset");
 var timerEl = $("#span-timer");
 var questionContainer = $("#question-container");
 var startTime = 10;
+var randomQuestion;
+var currentQuestion;
 
 btnStart.on("click", function () {
   timerEl.text(startTime);
@@ -13,27 +15,63 @@ btnStart.on("click", function () {
   var timer = setInterval(function () {
     countDown--;
     $("#span-timer").html(countDown);
-    if (countDown <= 0) {
-      $("#span-timer").html("TIME'S UP!");
+    if (countDown <= 5) {
+      $("#span-timer").css("color", "red");
     }
-    //ADD CODE HERE - IF TIMER < 6
-    //COLOR BECOMES RED
+    if (countDown <= 0) {
+      $("#span-timer").html("TIME'S UP!").css("font-size", "30px");
+    }
+    randomQuestion = question.sort(() => Math.random() - 0.5);
+    currentQuestion = 0;
     return timer;
   }, 1000);
   btnReset.on("click", function () {
     $("#btn-start").css("display", "inline");
     $("#btn-reset").css("display", "none");
     $("#question-container").addClass("hide");
-    $("#span-timer").html("Click 'Start' to begin the Quiz!");
+    $("#span-timer")
+      .css("font-size", "16px")
+      .css("color", "black")
+      .html("Click 'Start' to begin the Quiz!");
     clearInterval(timer);
   });
   //  REQUIRE 'REVEAL-QUESTION' FUNCTION HERE
 });
 
-// const myQuiz = [
-//   {
-//     q: "1. From which part of a plant does a fruit typically develop?",
-//     options: ["A. Leaf", "B. Flower", "C. Stem", "D. Roots"],
+//FIRST QUESTION TRIAL!!!
+const quizQuestions = [
+  {
+    question: "From which part of a plant does a fruit typically develop?",
+    answer: [
+      { text: "Leaf", correct: false },
+      { text: "Flower", correct: true },
+      { text: "Stem", correct: false },
+      { text: "Roots", correct: false },
+    ],
+  },
+];
+
+// question: "Which part of celery do we often eat?",
+// answer: [
+//         { text: "Leaf", correct: false },
+//       { text: "Flower", correct: false },
+//       { text: "Stem", correct: true },
+//       { text: "Roots", correct: false },
+//     ],
+//   },
+// ];
+
+// question: "Williams” and “Conference” are kinds of which type of fruit?";
+// answer: [
+//         { text: "Apples", correct: false },
+//       { text: "Bananas", correct: flase },
+//       { text: "Cucumbers", correct: false },
+//       { text: "Pears", correct: true },
+//     ],
+//   },
+// ];
+
+// "A. Leaf", "B. Flower", "C. Stem", "D. Roots"],
 //     correctIndex: 1,
 //     correctResponse: "Custom correct response.",
 //     incorrectResponse: "Custom incorrect response.",
