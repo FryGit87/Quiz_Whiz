83882f4a23532e70749929cda6435bb3be4b16f5
Kym Reilly <kymreilly@hotmail.com>
2022-03-31 10:50:42 +1030

    Quiz questions and answers added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ var startTime = 10;
 btnStart.on("click", function () {
   timerEl.text(startTime);
   $("#btn-start").css("display", "none");
-  // $("btn-start").css("display", "none");
   var countDown = startTime;
   var timer = setInterval(function () {
     countDown--;
@@ -27,4 +26,51 @@ btnStart.on("click", function () {
     clearInterval(timer);
   });
   //  REQUIRE 'REVEAL-QUESTION' FUNCTION HERE
+});
+
+const myQuiz = [
+  {
+    q: "1. From which part of a plant does a fruit typically develop?",
+    options: ["A. Leaf", "B. Flower", "C. Stem", "D. Roots"],
+    correctIndex: 1,
+    correctResponse: "Custom correct response.",
+    incorrectResponse: "Custom incorrect response.",
+  },
+  {
+    q: "2. Which part of celery do we often eat?",
+    options: ["A. Leaf", "B. Flower", "C. Stem", "D. Roots"],
+    correctIndex: 3,
+    correctResponse: "Custom correct response.",
+    incorrectResponse: "Custom incorrect response.",
+  },
+  {
+    q: "3. “Williams” and “Conference” are kinds of which type of fruit?",
+    options: ["A. Apples", "B. Bananas", "C. Cucumbers", "D. Pears"],
+    correctIndex: 3,
+    correctResponse: "Custom correct response.",
+    incorrectResponse: "Custom incorrect response.",
+  },
+  {
+    q: "4. This fruit is often grown in many Asian countries and is considered one of the stinkiest fruits in the world. What is it?",
+    options: ["A. Durian", "B. Rambutan", "C. Persimmon", "D. Lychee"],
+    correctIndex: 0,
+    correctResponse: "Custom correct response.",
+    incorrectResponse: "Custom incorrect response.",
+  },
+  {
+    q: "5. Broccoli, Brussels sprouts, cauliflower, and cabbage belong to which type of vegetables?",
+    options: [
+      "A. Root vegetables",
+      "B. Cruciferous vegetables",
+      "C. Leafy green vegetables",
+      "D. Poisonous vegetables",
+    ],
+    correctIndex: 1,
+    correctResponse: "Custom correct response.",
+    incorrectResponse: "Custom incorrect response.",
+  },
+];
+
+$("#quiz").quiz({
+  questions: myQuiz,
 });
