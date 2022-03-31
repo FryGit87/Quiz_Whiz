ececeb9ad2c28a964182166865938c78c6f28cb0
Kym Reilly <kymreilly@hotmail.com>
2022-04-01 00:17:00 +1030

    small animation added, extra score buttons added, score template added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Quiz_Whiz
+# **Quiz Whiz | Vegetable Quiz**
+
+## **App URL**
+
+https://frygit87.github.io/Quiz_Whiz/
+
+## **Goals**
+
+To build the world's greatest and most fabulously extraordinarily stupendously marvellous Vegetable Quiz.
+
+## **Tasks**
+
+Provide a user-friendly synopsis of the out-going, motivated and resourceful individual that is Kym Reilly.
+
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+    ✅  list of required tasks to complete, that have been completed
+
+## **Web App**
+
+![]()
+![]()
+![]()
+![]()
+![]()
+![]()
+![]()
+![]()
+![]()
--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ var btnStart = $("#btn-start");
 var btnReset = $("#btn-reset");
 var timerEl = $("#span-timer");
 var questionContainer = $("#question-container");
-var startTime = 10;
-var randomQuestion;
-var currentQuestion;
+var startTime = 5;
+var questionEl = $("#question");
+var answerButtonsEl = $("#answer-buttons");
+var question;
 
+//START BUTTON / RESET BUTTON / COUNTER
 btnStart.on("click", function () {
   timerEl.text(startTime);
   $("#btn-start").css("display", "none");
+  $("#btn-highScore").css("display", "none");
   $("#btn-reset").css("display", "inline-flex");
   $("#question-container").removeClass("hide");
   var countDown = startTime;
@@ -16,40 +19,49 @@ btnStart.on("click", function () {
     countDown--;
     $("#span-timer").html(countDown);
     if (countDown <= 5) {
+      $("#span-timer").fadeOut(400);
+      $("#span-timer").fadeIn(400);
       $("#span-timer").css("color", "red");
     }
     if (countDown <= 0) {
       $("#span-timer").html("TIME'S UP!").css("font-size", "30px");
+      $("#btn-saveScore").css("display", "inline");
+      $("#question-container").html("<h2>GAME OVER</h2>");
+      $("h2").append("<h4>Correct Answers: </h4>");
+      $("h2").append("<h4>Inorrect Answers: </h4>");
+      $("h2").append("<h4> SCORE: </h4>");
+      $("#btn-save").css("display", "inline");
     }
-    randomQuestion = question.sort(() => Math.random() - 0.5);
-    currentQuestion = 0;
     return timer;
   }, 1000);
   btnReset.on("click", function () {
     $("#btn-start").css("display", "inline");
+    $("#btn-highScore").css("display", "inline");
     $("#btn-reset").css("display", "none");
+    $("#btn-saveScore").css("display", "none");
     $("#question-container").addClass("hide");
     $("#span-timer")
-      .css("font-size", "16px")
+      .css("font-size", "20px")
       .css("color", "black")
       .html("Click 'Start' to begin the Quiz!");
     clearInterval(timer);
   });
-  //  REQUIRE 'REVEAL-QUESTION' FUNCTION HERE
 });
 
+//NEED TO ADD QUIZ FUNCTION, CALL WHEN START IS CLICKED
+
 //FIRST QUESTION TRIAL!!!
-const quizQuestions = [
-  {
-    question: "From which part of a plant does a fruit typically develop?",
-    answer: [
-      { text: "Leaf", correct: false },
-      { text: "Flower", correct: true },
-      { text: "Stem", correct: false },
-      { text: "Roots", correct: false },
-    ],
-  },
-];
+// const quizQuestions = [
+//   {
+//     question: "From which part of a plant does a fruit typically develop?",
+//     answer: [
+//       { text: "Leaf", correct: false },
+//       { text: "Flower", correct: true },
+//       { text: "Stem", correct: false },
+//       { text: "Roots", correct: false },
+//     ],
+//   },
+// ];
 
 // question: "Which part of celery do we often eat?",
 // answer: [
@@ -71,6 +83,18 @@ const quizQuestions = [
 //   },
 // ];
 
+// IF correct: true
+// correctAnswer++
+//else
+//wrongAnswer++
+// IF questionsAnswer == wrongAnswer + correctAnswer || countDown =< 0
+// $("#question-container").html("<h2>GAME OVER</h2>");
+// $("h2").append("<h4>Correct Answers: </h4>");
+// $("h2").append("<h4>Inorrect Answers: </h4>");
+// $("h2").append("<h4> Answers: </h4>");
+//$("#btn-save").css("display", "inline");
+
+// ----------------------------------------------------------------------------------------------
 // "A. Leaf", "B. Flower", "C. Stem", "D. Roots"],
 //     correctIndex: 1,
 //     correctResponse: "Custom correct response.",
