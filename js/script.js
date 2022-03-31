e49f3a928dcb93d614c7118cdc67cba332064e8d
Kym Reilly <kymreilly@hotmail.com>
2022-03-31 16:01:10 +1030

    Back to the drawing board, commit 83882f4a23532e70749929cda6435bb3be4b16f5

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
-var btnStart = $("#quiz-start-btn");
+var btnStart = $("#btn-start");
 var btnReset = $("#btn-reset");
 var timerEl = $("#span-timer");
 var startTime = 10;
 
 btnStart.on("click", function () {
   timerEl.text(startTime);
-  $("#quiz-start-btn").css("display", "none");
+  $("#btn-start").css("display", "none");
   var countDown = startTime;
   var timer = setInterval(function () {
     countDown--;
     $("#span-timer").html(countDown);
     if (countDown <= 0) {
       $("#span-timer").html("TIME'S UP!");
-      $(".question-container").css("display", "none");
-      $("#quiz-counter").css("display", "none");
     }
     //ADD CODE HERE - IF TIMER < 6
     //COLOR BECOMES RED
     return timer;
   }, 1000);
   btnReset.on("click", function () {
-    // $("#quiz-start-btn").css("display", "block");
-    $("#quiz-start-btn").removeAttr("style");
-
-    $("#quiz-start-screen").removeAttr("style");
-    $(".question-container").css("display", "none");
-    $("#quiz-counter").css("display", "none");
-
+    $("#btn-start").css("display", "block");
     $("#span-timer").html("Click 'Start' to begin the Quiz!");
     clearInterval(timer);
   });
